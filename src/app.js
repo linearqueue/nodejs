068386d248688a21d64f03784980d2db29449f0d
linearqueue <24204031154@stu.manit.ac.in>
2025-10-17 16:59:30 +0530

    server bnaya

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
-const express = require("express");
+const express = require("express"); //express require kia http server ke liye
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("hi this is shubham");
+app.use("/test", (req, res) => {
+  res.send("Running from the test");
+});
+
+app.use("/about", (req, res) => {
+  res.send("this is about page");
+});
+
+app.use("/", (req, res) => {
+  res.send("this is home page");
 });
 
 app.listen(3000, () => {
