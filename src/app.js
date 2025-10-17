9d32d87161c816e01089c7a2acdcc805ace1ff55
linearqueue <24204031154@stu.manit.ac.in>
2025-10-17 18:28:23 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const express = require("express"); //express require kia http server ke liye
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Running from the test");
+//  '/' === '/*'    '/' ke baad sab select kr lega
+//  /test === /test/*
+//  bydefault hm "GET" api call kr rhe on the above route
+
+// install postman
+
+// This will only handle GET method API Call
+app.get("/user", (req, res) => {
+  res.end("this is the user GET method");
 });
 
-app.use("/about", (req, res) => {
-  res.send("this is about page");
+app.post("/user", (req, res) => {
+  res.end("this is the user POST method");
+});
+
+app.delete("/user", (req, res) => {
+  res.end("this is the user DELETE method");
+});
+
+// this will use all the HTTP method API call to /test
+app.use("/test", (req, res) => {
+  res.end("this will use all the HTTP method API call to /test");
 });
 
 app.use("/", (req, res) => {
